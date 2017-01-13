2cdb2ee3a96b76e210cbf380110a5f93cd9453b4
Jens Kehne <jens@int80.de>
2017-01-13 20:17:56 +0100

    Initial import

--- js/data.js
+++ js/data.js
@@ -1,7 +1,7 @@
 
-var headerText = "Bingo";
+var headerText = "Goth-Bingo";
 
-var footerText = "<a href='https://github.com/jeffehobbs/HTML5-bingo/' target='_blank'>Code available on github</a>";
+var footerText = "Based on <a href='https://github.com/jeffehobbs/HTML5-bingo/' target='_blank'>HTML5-Bingo</a> by Jeff Hobbs";
 
 var winText = "Winner";
 
@@ -10,35 +10,53 @@ var clickSnd = new Audio("audio/click.mp3");
 var winSnd = new Audio("audio/win.mp3");
 
 var JSONBingo = {"squares": [
-        {"square": "one"},
-        {"square": "two"},
-        {"square": "three"},
-        {"square": "four"},
-        {"square": "five"},
-        {"square": "six"},
-        {"square": "seven"},
-        {"square": "eight"},
-        {"square": "nine"},
-        {"square": "ten"},
-        {"square": "eleven"},
-        {"square": "twelve"},
-        {"square": "thirteen"},
-        {"square": "fourteen"},
-        {"square": "fifteen"},
-        {"square": "sixteen"},
-        {"square": "seventeen"},
-        {"square": "eighteen"},
-        {"square": "nineteen"},
-        {"square": "twenty"},
-        {"square": "twenty one"},
-        {"square": "twenty two"},
-        {"square": "twenty three"},
-        {"square": "twenty four"},    	
-        {"square": "twenty five"},    	
-        {"square": "twenty six"},    	
-        {"square": "twenty seven"}, 
-        {"square": "twenty eight"},    	
-        {"square": "twenty nine"},    	
-        {"square": "thirty"}    	
+        {"square": "Fledermaus-Tatoo"},
+        {"square": "Engelsflügel"},
+        {"square": "New Rocks"},
+        {"square": "Kleidung mit Pelz"},
+        {"square": "Reifrock"},
+        {"square": "Leuchtstäbchen (Grün)"},
+        {"square": "Leuchtstäbchen (Gelb)"},
+        {"square": "Leuchtstäbchen (Rot)"},
+        {"square": "Cyberloxx (Grün)"},
+        {"square": "Cyberloxx (Gelb)"},
+        {"square": "Cyberloxx (Rot)"},
+        {"square": "Cyberloxx (Blau)"},
+        {"square": "Subway to Sally T-Shirt"},
+        {"square": "Frau in Netzoberteil"},
+        {"square": "Mann mit Rüschenhemd"},
+        {"square": "Mann oben ohne"},
+        {"square": "Frau oben ohne"},
+        {"square": "Mann in weiß"},
+        {"square": "Frau in weiß"},
+        {"square": "Mensch mit Fächer"},
+        {"square": "Frau mit schwarzem Lippenstift"},
+        {"square": "Mann mit schwarzem Lippenstift"},
+        {"square": "Dreadlocks"},
+        {"square": "Mann an der Leine"},    	
+        {"square": "Frau an der Leine"},    	
+        {"square": "Mann mit sehr langen Haaren"},    	
+        {"square": "Brustwarzenpiercing"}, 
+        {"square": "Mann in Frauenkleidung"},    	
+        {"square": "Vogel-Tattoo"},    	
+        {"square": "Frau mit Undercut"},    	
+        {"square": "Schweißerbrille"},    	
+        {"square": "Steampunk-Flügel"},    	
+        {"square": "Rosen-Tattoo"},    	
+        {"square": "Dämonenflügel"},    	
+        {"square": "Plateustiefel"},    	
+        {"square": "Kleidung mit Federn"},    	
+        {"square": "Mann mit Lederhose"},    	
+        {"square": "Tütü"},    	
+        {"square": "Frau mit sehr kurzen Haaren"},    	
+        {"square": "Mann mit Korsett"},    	
+        {"square": "Pentagramm-Tattoo"},    	
+        {"square": "Melone (Hut)"},    	
+        {"square": "Mann mit Iro"},    	
+        {"square": "Mann mit Rock"},    	
+        {"square": "Mundschutz"},    	
+        {"square": "Flockati-Jacke"},    	
+        {"square": "Steampunk-Kind"},    	
+        {"square": "Zylinder"}    	
     ]
 };
