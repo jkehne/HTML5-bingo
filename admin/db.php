f91ca88334aa86e4ef6e7e4c35908cde39d1f6fd
Jens Kehne <jens@int80.de>
2017-02-19 14:09:56 +0100

    Add admin tool for managing words

---
+++ admin/db.php
@@ -0,0 +1,78 @@
+<?php
+class database {
+    const HOST = "localhost";
+    const USER = "";
+    const PASSWORD = "";
+    const DATABASE = "";
+
+    private $db_handle;
+
+    function __construct() {
+        $this->db_handle = new mysqli(self::HOST, self::USER, self::PASSWORD, self::DATABASE);
+        if ($this->db_handle->connect_errno) {
+            echo "Failed to connect to MySQL: (" . $this->db_handle->connect_errno . ") " . $this->db_handle->connect_error;
+        }
+	if (!$this->db_handle->set_charset("utf8")) {
+	    printf("Error loading character set utf8: %s\n", $this->db_handle->error);
+	}
+    }
+
+    function __destruct() {
+        $this->db_handle->close();
+    }
+
+    public function insert_field($text) {
+        if ($sql = $this->db_handle->prepare("INSERT INTO fields (active, text) VALUES (1, ?)")) {
+            if ($sql->bind_param("s", $text)) {
+                $sql->execute();
+            } else {
+                echo "failed to bind parameter to insert statement";
+            }
+            $sql->close();
+        } else {
+            echo "Error preparing insert statement";
+        }
+    }
+
+    public function delete_field($id) {
+        if ($sql = $this->db_handle->prepare("DELETE FROM fields WHERE id=?")) {
+            if ($sql->bind_param("i", $id)) {
+                $sql->execute();
+            } else {
+                echo "failed to bind parameter to delete statement";
+            }
+            $sql->close();
+        } else {
+            echo "Error preparing delete statement";
+        }
+    }
+
+    public function set_field_active($id, $active) {
+    	if ($sql = $this->db_handle->prepare("UPDATE fields SET active=? WHERE id=?")) {
+            if ($sql->bind_param("ii", $active, $id)) {
+                $sql->execute();
+            } else {
+                echo "failed to bind parameter to update statement";
+            }
+            $sql->close();
+        } else {
+            echo "Error preparing update statement: " . $this->db_handle->error;
+        }
+    }
+
+    public function list_fields($active) {
+        $retval = array();
+
+        if (!$res = $this->db_handle->query("SELECT id, text FROM fields WHERE active = " . $active)) {
+            echo "Error querying available fields";
+            return NULL;
+        }
+
+        while ($row = $res->fetch_assoc()) {
+            array_push($retval, array("id" => $row['id'], "text" => $row['text']));
+        }
+
+        return $retval;
+    }
+}
+?>
---
+++ admin/index.php
@@ -0,0 +1,78 @@
+<?php
+require "db.php";
+
+$db = NULL;
+
+function init() {
+    global $db;
+    $db = new database();
+}
+
+function handle_action() {
+    global $db;
+
+    if (!isset($_POST['action']) && !isset($_GET['action']))
+        return;
+
+    $action = isset($_POST['action']) ? $_POST['action'] : $_GET['action'];
+
+    switch ($action) {
+    case "insert":
+        $db->insert_field($_POST['text']);
+        break;
+    case "delete":
+        $db->delete_field($_GET['id']);
+        break;
+    case "activate":
+        $db->set_field_active($_GET['id'], 1);
+        break;
+    case "deactivate":
+        $db->set_field_active($_GET['id'], 0);
+        break;
+    }
+}
+
+function output_table_row($id, $word, $active) {
+    echo "<tr><td>";
+    echo $word;
+    echo "</td><td>";
+    printf('<a href="index.php?action=%1$s&id=%2$d">%1$s</a>', $active ? "deactivate" : "activate", $id);
+    echo "&nbsp;";
+    printf('<a href="index.php?action=delete&id=%d">delete</a>', $id);
+    echo "</td></tr>";
+}
+
+function list_fields() {
+    global $db;
+    $fields = $db->list_fields(1);
+    foreach ($fields as $field) {
+        output_table_row($field['id'], $field['text'], 1);
+    }
+
+    $fields = $db->list_fields(0);
+    foreach ($fields as $field) {
+        output_table_row($field['id'], $field['text'], 0);
+    }
+}
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>WGT-Bingo admin area</title>
+  </head>
+  <body>
+    Insert new word:
+    <form action="index.php" method="post">
+      <input type="text" name="text">
+      <input type="hidden" name="action" value="insert">
+      <input type="submit" name="submit" value="Submit">
+    </form>
+    <table>
+    <?php
+      init();
+      handle_action();
+      list_fields();
+    ?>
+    </table>
+  </body>
+</html>
