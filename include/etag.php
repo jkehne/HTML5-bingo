451db3db9636a5e86c66201c739ea0652dbf6494
Jens Kehne <jens@int80.de>
2017-02-27 18:03:29 +0100

    Silence warning if client sends no ETag

--- include/etag.php
+++ include/etag.php
@@ -4,7 +4,7 @@ require_once "db.php";
 function do_etag($db) {
 	 $etag = $db->hash();
 
-	 if (trim($_SERVER['HTTP_IF_NONE_MATCH']) == $etag) {
+	 if (isset($_SERVER['HTTP_IF_NONE_MATCH']) && trim($_SERVER['HTTP_IF_NONE_MATCH']) == $etag) {
 	    header("HTTP/1.1 304 Not Modified");
 	    exit;
 	 }
